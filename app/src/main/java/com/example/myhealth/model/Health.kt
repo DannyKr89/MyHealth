6f633980db5195f84acaefe0d4fa0d03223a3eba
dk <kdi_2004@mail.ru>
2023-10-22 23:06:11 +0300

    Подключил DI Koin Подключил Realtime Database Добавил список отображающий давление и пульс пользователя в списке, разделенным по дням и часам

--- app/src/main/java/com/example/myhealth/model/Health.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myhealth.model
-
-data class Health(
-    val date: String,
-    val indications: List<Indication>
-)
--- app/src/main/java/com/example/myhealth/model/Indication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myhealth.model
-
-data class Indication(
-    val time: String,
-    val upperPressure: Int,
-    val lowerPressure: Int,
-    val pulse: Int
-)
